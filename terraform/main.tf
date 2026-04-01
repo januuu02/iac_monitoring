54a7bbb74b64468e465112c1dda7ce15f78e3501
januuu02 <128676707+januuu02@users.noreply.github.com>
2026-04-01 20:22:48 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,23 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "iac-vm"
+  machine_type = "e2-medium"
 
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    apt update
+    apt install -y docker.io docker-compose
+    systemctl start docker
+    systemctl enable docker
+  EOF
+}
