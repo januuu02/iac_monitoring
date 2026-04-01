7552a36eba40ceb05d9a150b510901ba3f27b480
januuu02 <128676707+januuu02@users.noreply.github.com>
2026-04-01 20:23:49 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,13 @@
+require('newrelic');
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Backend API hit");
+  res.send("Backend Running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
